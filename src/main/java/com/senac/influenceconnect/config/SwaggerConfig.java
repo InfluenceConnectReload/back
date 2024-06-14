080f1c4d4dcf57bcb1789fdeb33f20aad2a7c718
Alexandre Ferreira <ajpf44@gmail.com>
2024-06-14 14:46:57 -0300

    Fix: ajusta informações do swagger

--- src/main/java/com/senac/influenceconnect/config/SwaggerConfig.java
+++ src/main/java/com/senac/influenceconnect/config/SwaggerConfig.java
@@ -28,53 +28,13 @@ public class SwaggerConfig {
 		server.setDescription("Server URL - ambiente de desenvolvimento");
 
 		Contact contact = new Contact();
-		contact.setEmail("ajpf44@gmail.com");
-		contact.setName("Alexandre José Ponciano Ferreira");
-		contact.setUrl("https://ajpf44.github.io/");
-		
-//		Contact contact2 = new Contact();
-//		contact2.setEmail("deboramat.ipac@gmail.com");
-//		contact2.setName("Débora de Oliveira Souza");
-//		contact2.setUrl("http://www.aula-teste.com.br");
-//		
-//		Contact contact3 = new Contact();
-//		contact3.setEmail("deboramat.ipac@gmail.com");
-//		contact3.setName("Débora de Oliveira Souza");
-//		contact3.setUrl("http://www.aula-teste.com.br");
-//		
-//		Contact contact4 = new Contact();
-//		contact4.setEmail("deboramat.ipac@gmail.com");
-//		contact4.setName("Débora de Oliveira Souza");
-//		contact4.setUrl("http://www.aula-teste.com.br");
-//		
-//		Contact contact5 = new Contact();
-//		contact5.setEmail("deboramat.ipac@gmail.com");
-//		contact5.setName("Débora de Oliveira Souza");
-//		contact5.setUrl("http://www.aula-teste.com.br");
-//		
-//		Contact contact6 = new Contact();
-//		contact6.setEmail("deboramat.ipac@gmail.com");
-//		contact6.setName("Débora de Oliveira Souza");
-//		contact6.setUrl("http://www.aula-teste.com.br");
-//		
-//		Contact contact7 = new Contact();
-//		contact7.setEmail("deboramat.ipac@gmail.com");
-//		contact7.setName("Débora de Oliveira Souza");
-//		contact7.setUrl("http://www.aula-teste.com.br");
-//		
-//		contacts.addAll(List.of(
-//		        contact,
-//		        contact2,
-//		        contact3,
-//		        contact4,
-//		        contact5,
-//		        contact6,
-//		        contact7
-//		    ));
+		contact.setEmail("petropolis@rj.senac.br");
+		contact.setName("Grupo Influence Connect");
+		contact.setUrl("https://github.com/InfluenceConnect/back");
 
-		Info info = new Info().title("Documentação API - Aula Teste SENAC Petrópolis").version("1.0.0").contact(contact)
-				.description("API com endpoints da Aula Teste.")
-				.termsOfService("http://www.aula-teste.com.br/terms");
+		Info info = new Info().title("Documentação API - Info 4 -Influence Connect").version("1.0.0").contact(contact)
+				.description("API com endpoints do backend do projeto Influence Connect.")
+				.termsOfService("https://github.com/InfluenceConnect/back/blob/main/LICENSE");
 
 		return new OpenAPI().info(info).servers(List.of(server));
 	}
